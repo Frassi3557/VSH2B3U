50beee651bf6d4d2a87971f2fd471b1476533322
Snorri Freyr Ásgeirsson <freysi_freys@live.com>
2016-11-13 23:33:05 +0000

    Add files via upload

---
+++ Verkefni_5/index.php
@@ -0,0 +1,141 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="stylesheets/stylesheet.css">
+        <link rel="stylesheet" href="stylesheets/normalize.css">
+			<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+			<link rel="stylesheet" href="icons/style.css">
+        <title>Verkefni 5</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body>
+			<header>
+      	<div>
+					<input class="input" type="checkbox" id="toggle">
+        	<label class="checkbox" for="toggle">&#9776;  Verkefni 5</label>
+				<nav class="menu">
+					<ul class="topmenu">
+						<a href="#i1"><li>Síða 1</li></a>
+						<a href="#i2"><li>Síða 2</li></a>
+						<a href="#i3"><li>Síða 3</li></a>
+						<a href="#i4"><li>Síða 4</li></a>
+						<a href="http://tsuts.tskoli.is/2t/0506992429/"><li>Efnisyfirlit</li></a>
+					</ul>
+				</nav>
+				</div>
+      </header>
+			
+				<div class="back-img img1"></div>
+        <div id="i1" class="page">
+					<div class="content">
+						<h1><div class="icon-home"></div>Harpan eða eitthvað</h1>
+						<div class="artists">
+							<div class="col-1-3"><h2>John Coltrane</h2><p>John William Coltrane, also known as "Trane", was an American jazz saxophonist and composer. Working in the bebop and hard bop idioms early in his career, Coltrane helped pioneer the use of modes in jazz and was later at the forefront of free jazz.</p></P></div>
+							<div class="col-1-3"><h2>Miles Davis</h2><p>Miles Dewey Davis III was an American jazz trumpeter, bandleader, and composer. He is among the most influential and acclaimed figures in the history of jazz and 20th century music</p></div>
+							<div class="col-1-3"><h2>Duke Ellington</h2><p>Edward Kennedy "Duke" Ellington was an American composer, pianist, and bandleader of a jazz orchestra, which he led from 1923 until his death in a career spanning over fifty years</p></div>
+						</div>
+					</div>
+					</div>
+					
+        
+			
+			
+				<div class="back-img img2"></div>
+        <div id="i2" class="page">
+					<div class="content">
+						<h1><div class="icon-man-woman"></div>Once in a lifetime experience</h1>
+						<p>
+							Original Message<br>
+							From: H [mailto:HDR22@clintonemail.com]<br>
+							<br>
+							Sent: 15 June 2010 02:35<br>
+							To: Cherie Blair<br>
+							Subject: Re: confidential<br>
+							<br>
+							<br>
+							I would be happy to meet w him. How should I follow up to arrange a mutually agreeable time? All the best to you.<br>
+							<br>
+							Original Message<br>
+							From: Cherie Blair cb<br>
+							To: H<br>
+							<br>
+							Sent: Mon Jun 07 09:56:45 2010<br>
+							Subject: confidential<br>
+							<br>
+							<br>
+							As you know I have good links to the Qataris. Sheikha Mosah's son is the 31/32 year old Crown Prince and she is keen
+							that he starts to build a wider international profile.<br> He has been appointed head of the Qatar National Food Security
+							
+							Programme which is designed to deal with Qatar's food security issues its a $30 billion programme. <br>He is aware that
+							food security is an issue you are majoring on and would love to meet with you in person to discuss this and to explore
+							potential partnership areas between US and Qatar including possible participation in the US/World Bank Food Security
+							
+							Fund. <br>He is also keen to talk more generally about US/Qatari cooperation and even suggested they could help out with
+							the Oil spill problem. There is apparently a Arab Food Security Summit in October 2010 and he would like to discuss Us
+							participation in that too.<br>
+							<br>
+							UNCLASSIFIED U.S. Department of State Case No. F-2014-20439 Doc No. C05777269 Date: 08/31/2015<br>
+							<br>
+							IS a meeting possible he is more flexible on place and times than his mother! Or maybe we could set up an initial phone<br>
+							<br>
+							call.<br>
+							<br>
+							Can you let me know if this appeals to you at all?<br>
+							<br>
+							Cherie. Blair<br>
+							<br>
+							This message has been scanned by MailController - www.MailController.altohiway.com</p>
+					</div>
+					
+					
+					
+				<div class="back-img img3"></div>
+        <div id="i3" class="page">
+        	<div class="content">
+						<h1><div class="icon-calendar"></div>Venues</h1>
+					</div>    
+        </div>
+					
+					
+				<div class="back-img img4"></div>
+        <div id="i4" class="page">
+          <div class="content">
+						<h1><div class="icon-map"></div>How to get there ?</h1>
+						<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1739.6059651928344!2d-21.934890783499934!3d64.15033658359694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48d674d3019cb491%3A0xa813064fd5697156!2sHarpa+Concert+Hall+and+Conference+Centre!5e0!3m2!1sen!2sis!4v1478775677682" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+						<h2 class="directions">The Harpa is hard to miss</h2>
+						<p class="directions">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae risus libero. Nullam mollis, risus non eleifend iaculis, tellus leo cursus orci, quis mollis turpis odio vehicula neque. Morbi nisl eros, cursus sit amet lectus at, iaculis consectetur orci. Vivamus in vulputate massa, vitae blandit lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie nulla sed euismod porttitor.<br><br> Suspendisse a augue leo. Praesent egestas et mi bibendum aliquam. Nullam posuere consequat lorem, ut lacinia urna blandit et. Mauris nisi dui, blandit vitae nulla sed, luctus placerat mauris. In mollis convallis ligula sed volutpat. Proin ornare sollicitudin nulla sed efficitur. Praesent dapibus condimentum odio, sit amet scelerisque leo malesuada sit amet.</p>
+					</div> 
+				</div>
+					
+					<footer class="footer">
+						<a class="i-3" href="#i1">Fyrsta síða</a>
+						<a class="i-3" href="http://tsuts.tskoli.is/2t/0506992429/">Efnisyfirlit</a>
+						<a class="i-3" href="https://github.com/Frassi3557/VSH2B3U/tree/master/Verkefni_4">Github</a>
+    			</footer>
+        </div>
+    </body>
+    <script>
+			$(document).ready(function(){
+				// Add smooth scrolling to all links
+				$("a").on('click', function(event) {
+					// Make sure this.hash has a value before overriding default behavior
+					if (this.hash !== "") {
+						// Prevent default anchor click behavior
+						event.preventDefault();
+						// Store hash
+						var hash = this.hash;
+						// Using jQuery's animate() method to add smooth page scroll
+						// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+						$('html, body').animate({
+							scrollTop: $(hash).offset().top
+						}, 1000, function(){
+							
+							// Add hash (#) to URL when done scrolling (default click behavior)
+							window.location.hash = hash;
+						});
+					} // End if
+				});
+			});
+		</script>
+</html>
